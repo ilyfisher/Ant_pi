d682d3646746e4e5680ef71100386d9e2863569e
Ily Fisher <ilyfisher99@gmail.com>
2021-05-08 20:07:27 -0400

    Updated temperature in farenheit and fixed some API key errors

--- test.py
+++ test.py
@@ -6,21 +6,21 @@ import requests
 
 #dht = DHT22(board.D4)
 dht = adafruit_dht.DHT22(board.D4, use_pulseio=False)
+thingspeak_key = '932RYNZS3Z7NNRVO'
 
 while True:
     try:
-        temperature, humidity = dht.temperature, dht.humidity
-        print(temperature, humidity)
-        
-        # If either reading has failed after repeated retries,
-        # abort and log message to ThingSpeak
-        thingspeak_key = '932RYNZS3Z7NNRVO'
+        humidity = dht.humidity      
+        temperature = ((dht.temperature*(9/5)) + 32)
+        temperature = round(temperature,1)
+        print(temperature,"f ", humidity, "%")
+                
         if humidity is None or temperature is None:
-            f = requests.post('https://api.thingspeak.com/update.json', data = {'932RYNZS3Z7NNRVO':thingspeak_key, 'status':'failed to get reading'})
+            f = requests.post('https://api.thingspeak.com/update.json', data = {'api_key':thingspeak_key, 'status':'failed to get reading'})
         else:
             # Send the data to Thingspeak
-            r = requests.post('https://api.thingspeak.com/update.json', data = {'932RYNZS3Z7NNRVO':thingspeak_key, 'field1':temperature, 'field2':humidity})
-    
+            r = requests.post('https://api.thingspeak.com/update.json', data = {'api_key':thingspeak_key, 'field1':temperature, 'field2':humidity})
+            
     except:
         time.sleep(2)
 
